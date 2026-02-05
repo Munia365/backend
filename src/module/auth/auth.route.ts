c9e166bcb2bb314590e4795aca9bba64d72123af
Munia365 <muniasultana6826@gmail.com>
2026-02-05 20:00:52 +0600

    feat(auth): add user management routes and services

---
+++ src/module/auth/auth.route.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { userController } from "./auth.controller";
+import authGuard from "../../guard/auth.guard";
+import { ROLE } from "../../generated/prisma/enums";
+const router = Router();
+router.get("/admin/users", authGuard(ROLE.ADMIN), userController.getAllUsers);
+router.get("/auth/me", authGuard(), userController.getCurrentUser);
+router.patch(
+  "/admin/users/:id",
+  authGuard(ROLE.ADMIN),
+  userController.updateUser,
+);
+router.delete(
+  "/admin/users/:userId",
+  authGuard(ROLE.ADMIN),
+  userController.deleteUserByAdmin,
+);
+router.delete("/users/me", authGuard(), userController.deleteMyAccount);
+export const userRouter = router;
---
+++ src/module/auth/auth.service.ts
@@ -0,0 +1,61 @@
+import { USER_STATUS } from "../../generated/prisma/enums";
+import { prisma } from "../../lib/prisma";
+const getAllUsers = async () => {
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw new Error("Failed to fetch users from database");
+  }
+};
+const getCurrentUser = async (id: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw new Error("Failed to fetch users from database");
+  }
+};
+const updatedUser = async (id: string, status: USER_STATUS) => {
+  try {
+    const result = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        status: status,
+      },
+    });
+    return result;
+  } catch (error) {
+    console.error("Error Updating users:", error);
+    throw new Error("Failed to Update users from database");
+  }
+};
+const deleteUser = async (userId: string) => {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return prisma.user.update({
+    where: { id: userId },
+    data: {
+      status: USER_STATUS.SUSPENDED,
+    },
+  });
+};
+export const userService = {
+  getAllUsers,
+  getCurrentUser,
+  updatedUser,
+  deleteUser,
+};
